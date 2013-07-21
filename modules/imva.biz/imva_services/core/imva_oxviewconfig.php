e0f8b418084af730734d259a9cff85bdf4b834bc
Johannes Ackermann <ja@imva.biz>
2013-07-21 13:17:57 +0200

    upd 0.1.2.5

--- modules/imva.biz/imva_services/core/imva_oxviewconfig.php
+++ modules/imva.biz/imva_services/core/imva_oxviewconfig.php
@@ -1,36 +1,54 @@
 <?php
 
 /**
- * IMVA Module Services: OxViewconfig extension
- * 
- * Redistribution permitted.
- * 
- * Weitergabe verboten.
+ * IMVA Module Services: OxViewConfig Extension
  * 
  *
- * This Software is intellectual property of imva.biz respectively of its author and is protected
- * by copyright law. This software product is open-source, but not freeware.
  *
- * Any unauthorized use of this software product - usage without a valid license,
- * modification, copying, redistribution, transmission is a violation of the license agreement
- * and will be prosecuted by civil and criminal law.
+ * For redistribution in the provicer's network only.
+ *
+ * Weitergabe außerhalb des Anbieternetzwerkes verboten.
  * 
- * By applying and using this software product, you agree to the terms and condition of usage and
- * furthermore agree, not to share information, source codes, technologies, credentials and addresses
- * of any kind.
+ *
+ *
+ * This software is intellectual property of imva.biz respectively of its author and is protected
+ * by copyright law. This software product is provided "as it is" with no guarantee.
+ *
+ * You are free to use this software and to modify it in order to fit your requirements.
  * 
- *  
+ * Any modification, copying, redistribution, transmission outsitde of the provider's platforms
+ * is a violation of the license agreement and will be prosecuted by civil and criminal law.
+ *
+ * By applying and using this software product, you agree to the terms and conditions of use.
+ * 
+ * 
+ * 
+ * Diese Software ist geistiges Eigentum von imva.biz respektive ihres Autors und ist durch das
+ * Urheberrecht geschützt. Diese Software wird ohne irgendwelche Garantien und "wie sie ist"
+ * angeboten.
+ * 
+ * Sie sind berechtigt, diese Software frei zu nutzen und auf Ihre Bedürfnisse anzupassen.
+ * 
+ * Jegliche Modifikation, Vervielfältigung, Redistribution, Übertragung zum Zwecke der
+ * Weiterentwicklung außerhalb der Netzwerke des Anbieters ist untersagt und stellt einen Verstoß
+ * gegen die Lizenzvereinbarung dar.
+ *
  * Mit der Übernahme dieser Software akzeptieren Sie die zwischen Ihnen und dem Herausgeber
- * festgehaltenen Bedingungen und wahren Stillschweigen über die Ihnen zugänglich gemachten
- * Informationen, Quellcodes, Technologien, Zugangsdaten und Adressen jeglicher Art.
- * Der Bruch dieser Bedingungen kann Schadensersatzforderungen nach sich ziehen.
+ * festgehaltenen Bedingungen. Der Bruch dieser Bedingungen kann Schadensersatzforderungen nach
+ * sich ziehen.
  * 
- * (c) 2013 imva.biz, Johannes Ackermann, ja@imva.biz
+ * 
+ * 
+ * (EULA-13/7)
+ * 
+ * 
+ *
+ * (c) 2012-2013 imva.biz, Johannes Ackermann, ja@imva.biz
  * @author Johannes Ackermann
- * 
- * 13/6/28
- * V 0.1
- * 
+ *
+ * 13/6/28-7/19
+ * V 0.1.2.5
+ *
  */
 
 class imva_oxviewconfig extends imva_oxviewconfig_parent{
